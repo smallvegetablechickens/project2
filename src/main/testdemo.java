65f693241cd8313965d3255d700ae24d1d979da7
liuzq <544022609@qq.com>
2019-12-11 14:58:51 +0800

    一次提交

--- src/main/testdemo.java
+++ src/main/testdemo.java
@@ -3,5 +3,6 @@ package main;
 public class testdemo {
     public static void main(String[] args) {
         System.out.println("this is project2");
+        System.out.println("this is project3");
     }
 }
