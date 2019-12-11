185934e89faded99e31772257b1ee549cb6b41c8
liuzq <544022609@qq.com>
2019-12-11 15:32:27 +0800

    二次提交

--- src/main/testdemo.java
+++ src/main/testdemo.java
@@ -2,8 +2,6 @@ package main;
 
 public class testdemo {
     public static void main(String[] args) {
-        System.out.println("this is project5");
-        System.out.println("this is project4");
-        System.out.println("this is project6");
+        System.out.println("第四次提交");
     }
 }
